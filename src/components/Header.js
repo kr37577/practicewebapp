5e572b6fc8fb823f53ee712cd3bd9c529fd5ffb1
kr37577 <jiatenl127@gmail.com>
2024-05-06 22:15:16 +0900

    ポートフォリオ

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <h3>ポートフォリオ</h3>
+        </div>
+         <nav>
+          <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">EC</a>
+            </li>
+            <li>
+                <a href="#">SNS</a>
+            </li>
+          </ul>
+         </nav>
+    </header>
+  )
+}
+
+export default Header
